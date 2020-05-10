b76307f60980c802c347d844c2047831bb2ed107
Breno Henrique <hennrique.breno@gmail.com>
2020-05-10 16:00:04 -0300

    feat(spec/support/request): adiciona metodos ao helper
    foi adicionado metodos para ajudar na montagem dos headers das requisicoes

--- spec/support/request_spec_helper.rb
+++ spec/support/request_spec_helper.rb
@@ -2,4 +2,24 @@ module RequestSpecHelper
   def json
     JSON.parse(response.body)
   end
+
+  def token_generator(account_id)
+    JsonWebToken.encode(account_id: account_id)
+  end
+
+  def expired_token_generator(account_id)
+    JsonWebToken.encode({ account_id: account_id }, (Time.now.to_i - 10))
+  end
+
+  def valid_headers
+    {
+      "Authorization" => "Bearer #{token_generator(account.id)}"
+    }
+  end
+
+  def invalid_headers
+    {
+      "Authorization" => nil
+    }
+  end
 end
